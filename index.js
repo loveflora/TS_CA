2a4126f8e4a7063251e178efada66aab76f4e321
sehwa <love7620640@naver.com>
2023-04-04 18:02:19 +0900

    0404

--- index.js
+++ index.js
@@ -31,3 +31,30 @@ var 제목 = document.querySelector("#title");
 if (제목 != null) {
     제목.innerHTML = "반가워요";
 }
+var User = /** @class */ (function () {
+    function User() {
+        this.intro = User.skill + "전문가입니다";
+    }
+    User.skill = "js";
+    return User;
+}());
+var 철수 = new User();
+console.log(철수); // { intro : "undefined 전문가입니다"}
+User.skill = "python";
+var 민수 = new User();
+console.log(민수);
+// class UserNum {
+//   private static x = 10;
+//   public static y = 20;
+//   addOne(a) {
+//     this.x += a;
+//   }
+// }
+// UserNum.addOne(3); //이렇게 하면 x가 3 더해져야함
+// UserNum.addOne(4); //이렇게 하면 x가 4 더해져야함
+// UserNum.printX(); //이렇게 하면 콘솔창에 x값이 출력되어야함
+// let 네모 = new Square(30, 30, "red");
+// 네모.draw();
+// 네모.draw();
+// 네모.draw();
+// 네모.draw();
--- index.ts
+++ index.ts
@@ -45,6 +45,37 @@ function 함수(a: string, func1: 함수타입1, func2: 함수타입2) {
 함수("010-1111-2222", cutZero, removeDash);
 
 let 제목 = document.querySelector("#title");
+
 if (제목 != null) {
   제목.innerHTML = "반가워요";
 }
+
+class User {
+  static skill = "js";
+  intro = User.skill + "전문가입니다";
+}
+
+var 철수 = new User();
+console.log(철수); // { intro : "undefined 전문가입니다"}
+
+User.skill = "python";
+var 민수 = new User();
+console.log(민수);
+
+// class UserNum {
+//   private static x = 10;
+//   public static y = 20;
+
+//   addOne(a) {
+//     this.x += a;
+//   }
+// }
+// UserNum.addOne(3); //이렇게 하면 x가 3 더해져야함
+// UserNum.addOne(4); //이렇게 하면 x가 4 더해져야함
+// UserNum.printX(); //이렇게 하면 콘솔창에 x값이 출력되어야함
+
+// let 네모 = new Square(30, 30, "red");
+// 네모.draw();
+// 네모.draw();
+// 네모.draw();
+// 네모.draw();
